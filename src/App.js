bf06de9fe24660a7b90f90e3b4c31c105a7ce50b
OmegSt4r <144837195+OmegSt4r@users.noreply.github.com>
2023-11-16 10:15:33 -0500

    f

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Hero from './Components/Hero';
-import imageSrc from './Components/Assets/todo.jpg';
+import image from './Components/img/todo.jpg';
 function App() {
   const heroData = {
-    imageSrc: './Components/Assets/todo.jpg',
+    image: './Components/img/todo.jpg',
     heading: 'Hello World',
     subHeading: 'Welcome to my app by using props',
     buttonText: 'Learn More',
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import image from './img/todo.jpg';
 function Hero(props) {
   const { imageSrc, heading, subHeading, buttonText } = props.data;
 
   return (
     <div className="hero">
-      <img src={imageSrc} alt="Hero"/>
+      <img src={image} alt="Hero"/>
       <h1>{heading}</h1>
       <h2>{subHeading}</h2>
       <button>{buttonText}</button>
